f518b006df0841f798672db6013bf09a3b29a3dd
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2023-01-17 08:08:13 +0000

    Update post_detai.html to view posts and comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = \
     'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STARTICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/Media/'
@@ -146,3 +146,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
